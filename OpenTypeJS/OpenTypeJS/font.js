e61b019e1b5c0285794aa3d75972387f5b42a2f9
Tony <show_3@163.com>
2019-11-13 09:18:02 +0800

    文本行高计算

--- OpenTypeJS/OpenTypeJS/font.js
+++ OpenTypeJS/OpenTypeJS/font.js
@@ -1,11 +1,7 @@
-var TOFont = {
-    fontList : {},
-    init : function(fontList,callback){
-        this.loadFonts(this,fontList,0,function(error){
-                       callback(error);
-                       });
-    },
-    loadFonts : function(toFont,fontList,index,callback){
+function TOFont (fontList,callback) {
+    this.fontList = {};
+    
+    this.loadFonts = function(toFont,fontList,index,callback){
         if (index < fontList.length){
             var path = fontList[index];
             opentype.load(path,function(error,font){
@@ -20,20 +16,89 @@ var TOFont = {
         }else{
             callback(null);
         }
-    },
-    readGlyph : function(char){
+    };
+    this.readGlyph = function(char){
         for (var item in this.fontList){
-            var glyph = item.charToGlyph(char)
-            if (glyph.unicode != undefine){
+            var glyph = this.fontList[item].charToGlyph(char)
+            if (glyph.unicode != undefined){
                 return glyph;
             }
         }
         return null;
-    }
+    };
+    
+    
+    this.loadFonts(this,fontList,0,function(error){
+                   callback(error);
+                   });
+    
 };
-var TOParagraph = {
+function TOParagraph (str,font,width,fontSize = 14) {
+    this.str = str;
+    this.font = font;
+    this.width = width;
+    this.fontSize = fontSize;
+    
+    this.readLines = function (width){
+        var strArr = Array.from(this.str);
+        var lines = [];
+        var linePaths = [];
+        var lineWidth = 0;
+        var lineStr = "";
+        
+        for (i=0;i<strArr.length;i++){
+            tempStr = strArr[i];
+            glyph = this.font.readGlyph(tempStr);
+            
+            if (!glyph){
+                continue;
+            }
+            path = glyph.getPath(0, 0, this.fontSize);
+            box = path.getBoundingBox();
+            if (lineWidth + box.x2 < width){
+                lineWidth += box.x2;
+                linePaths.push(path);
+                lineStr += tempStr;
+            }else{
+                lines.push(new TOLine(linePaths,lineStr));
+                linePaths = [];
+                lineWidth = 0;
+                lineStr = "";
+            }
+        }
+        if (lineStr.length > 0){
+            lines.push(new TOLine(linePaths,lineStr));
+        }
+        
+        return lines;
+    }
+    this.lines = this.readLines(this.width);
+    
+    this.getSize = function (){
+        var height = 0;
+        for (var key in this.lines){
+            var line = this.lines[key];
+            height += line.getSize().height;
+        }
+        return {"width":this.width,"height":height};
+
+    }
     
 };
 
-var TOLine = {
+function TOLine (paths,str) {
+    this.lineStr = str;
+    this.paths = paths;
+    this.getSize = function (){
+        var width = 0,height = 0;
+        for (var key in this.paths){
+            var path = this.paths[key];
+            box = path.getBoundingBox();
+            if (box.y2 > height){
+                height = box.y2;
+            }
+            width += box.x2;
+        }
+        return {"width":width,"height":height};
+    }
 }
