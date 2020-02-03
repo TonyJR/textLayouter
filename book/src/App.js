067d5c8f4673822b172a57727c8be5e0510d94a5
TonyJR <qintao@shiqichuban.com>
2020-02-03 17:33:32 +0800

    测试

--- book/src/App.js
+++ book/src/App.js
@@ -5,7 +5,7 @@ import FlipPage from 'react-flip-page'
 function App() {
   let blockList = [
     {id:"1",url:"https://gss0.baidu.com/9fo3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D650%2C867/sign=28b97b2584d4b31cf0699cbdb2e60b47/d000baa1cd11728b2544ec44c7fcc3cec3fd2c6e.jpg",type:1,x:100,y:100,width:100,height:100},
-    {id:"0",text:"😄😄123456789012345678901234567890123456789012345678901234567890",type:0,x:100,y:100,width:100},
+    {id:"0",text:"😄😄123456789012345678901234567890123456789012345678901234567890",type:0,x:200,y:100,width:100},
   ];
   return (
     <FlipPage orientation={"horizontal"} flipOnTouch={true}>
